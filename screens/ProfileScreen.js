752cb5946debb264b7102bc0fba88e7965633574
esraozdemir6 <esraozddemir@gmail.com>
2025-12-11 14:49:50 +0300

    feat: implement responsive layout with useWindowDimensions ,→ (Part 5)

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  useWindowDimensions,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { COLORS, SPACING, RADII, FONTS } from '../theme';
 
 export default function ProfileScreen() {
   const [theme, setTheme] = useState('light');
   const currentTheme = COLORS[theme];
+
+  // 🔹 Responsive logic
+  const { width } = useWindowDimensions();
+  const isLargeScreen = width > 500;
 
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
@@ -31,12 +41,16 @@ export default function ProfileScreen() {
       <View
         style={[
           styles.card,
-          { backgroundColor: currentTheme.card },
+          {
+            backgroundColor: currentTheme.card,
+            padding: isLargeScreen ? SPACING.xl : SPACING.lg,
+            width: isLargeScreen ? '60%' : '85%',
+          },
         ]}
       >
         <Ionicons
           name="person-circle-outline"
-          size={80}
+          size={isLargeScreen ? 100 : 80}
           color={currentTheme.text}
         />
 
@@ -89,10 +103,8 @@ const styles = StyleSheet.create({
   },
 
   card: {
-    width: '85%',
     borderRadius: RADII.md,
     alignItems: 'center',
-    padding: SPACING.lg,
     // iOS shadow
     shadowColor: '#000',
     shadowOpacity: 0.15,
